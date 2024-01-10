050fb5bbde60f8b0ff881f78ddc930efd339c4b6
kafeikui <kkffkk3@protonmail.com>
2024-01-10 12:07:37 +0800

    add test cases

--- test/Sender.t.sol
+++ test/Sender.t.sol
@@ -11,6 +11,8 @@ contract SenderTest is Test {
     MockERC20 public mock1;
     MockERC20 public mock2;
     address public user = address(0x111111111111);
+    address public receiver1 = address(0x222222222222);
+    address public receiver2 = address(0x333333333333);
 
     function setUp() public {
         vm.prank(user);
@@ -22,8 +24,8 @@ contract SenderTest is Test {
     function testSendToken() public {
         address[] memory receivers = new address[](2);
         uint256[] memory balances = new uint256[](2);
-        receivers[0] = address(0x90000000000000001);
-        receivers[1] = address(0x90000000000000002);
+        receivers[0] = receiver1;
+        receivers[1] = receiver2;
         balances[0] = 100;
         balances[1] = 200;
 
@@ -58,8 +60,8 @@ contract SenderTest is Test {
         address[] memory receivers = new address[](2);
         uint256[] memory token1Balances = new uint256[](2);
         uint256[] memory token2Balances = new uint256[](2);
-        receivers[0] = address(0x90000000000000001);
-        receivers[1] = address(0x90000000000000002);
+        receivers[0] = receiver1;
+        receivers[1] = receiver2;
         token1Balances[0] = 100;
         token1Balances[1] = 200;
         token2Balances[0] = 10;
@@ -108,8 +110,8 @@ contract SenderTest is Test {
     function testSendEther() public {
         address[] memory receivers = new address[](2);
         uint256[] memory balances = new uint256[](2);
-        receivers[0] = address(0x90000000000000001);
-        receivers[1] = address(0x90000000000000002);
+        receivers[0] = receiver1;
+        receivers[1] = receiver2;
         balances[0] = 10;
         balances[1] = 20;
 
@@ -137,8 +139,8 @@ contract SenderTest is Test {
         address[] memory receivers = new address[](2);
         uint256[] memory tokenBalances = new uint256[](2);
         uint256[] memory etherBalances = new uint256[](2);
-        receivers[0] = address(0x90000000000000001);
-        receivers[1] = address(0x90000000000000002);
+        receivers[0] = receiver1;
+        receivers[1] = receiver2;
         tokenBalances[0] = 100;
         tokenBalances[1] = 200;
         etherBalances[0] = 10;
@@ -178,40 +180,26 @@ contract SenderTest is Test {
     }
 
     function testClaimTokens() public {
-        address[] memory receivers = new address[](2);
-        uint256[] memory balances = new uint256[](2);
-        receivers[0] = address(0x90000000000000001);
-        receivers[1] = address(0x90000000000000002);
-        balances[0] = 10;
-        balances[1] = 20;
-
-        vm.deal(user, 3000);
+        vm.deal(address(sender), 300);
         vm.prank(user);
-
-        assertEq(user.balance, 3000);
-        assertEq(receivers[0].balance, 0);
-        assertEq(receivers[1].balance, 0);
-
-        printUserBalance(user);
-        emit log_string("-----------------------------------------------");
-
-        vm.prank(user);
-        sender.sendEther{value: 300}(receivers, balances);
-        assertEq(user.balance, 2700);
-        assertEq(receivers[0].balance, 10);
-        assertEq(receivers[1].balance, 20);
+        mock1.mint(address(sender), 3000);
+        assertEq(user.balance, 0);
+        assertEq(mock1.balanceOf(user), 0);
 
         vm.prank(user);
         sender.claimTokens(address(0x0));
-        assertEq(user.balance, 2970);
+        assertEq(user.balance, 300);
+        vm.prank(user);
+        sender.claimTokens(address(mock1));
+        assertEq(mock1.balanceOf(user), 3000);
         printUserBalance(user);
     }
 
     function testNotEnoughTokenAllowance() public {
         address[] memory receivers = new address[](2);
         uint256[] memory balances = new uint256[](2);
-        receivers[0] = address(0x90000000000000001);
-        receivers[1] = address(0x90000000000000002);
+        receivers[0] = receiver1;
+        receivers[1] = receiver2;
         balances[0] = 10;
         balances[1] = 20;
 
@@ -237,8 +225,8 @@ contract SenderTest is Test {
     function testNotEnoughTokenBalance() public {
         address[] memory receivers = new address[](2);
         uint256[] memory balances = new uint256[](2);
-        receivers[0] = address(0x90000000000000001);
-        receivers[1] = address(0x90000000000000002);
+        receivers[0] = receiver1;
+        receivers[1] = receiver2;
         balances[0] = 10;
         balances[1] = 20;
 
@@ -259,11 +247,11 @@ contract SenderTest is Test {
         sender.sendToken(address(mock1), receivers, balances);
     }
 
-    function testNotEnoughEtherBalance() public {
+    function testNotEnoughEther() public {
         address[] memory receivers = new address[](2);
         uint256[] memory balances = new uint256[](2);
-        receivers[0] = address(0x90000000000000001);
-        receivers[1] = address(0x90000000000000002);
+        receivers[0] = receiver1;
+        receivers[1] = receiver2;
         balances[0] = 10;
         balances[1] = 20;
 
@@ -274,8 +262,111 @@ contract SenderTest is Test {
         assertEq(receivers[1].balance, 0);
 
         vm.prank(user);
-        vm.expectRevert(abi.encodeWithSelector(Sender.NotEnoughBalance.selector));
+        vm.expectRevert(abi.encodeWithSelector(Sender.NotEnoughEther.selector));
         sender.sendEther{value: 15}(receivers, balances);
+    }
+
+    function testTooMuchEther() public {
+        address[] memory receivers = new address[](2);
+        uint256[] memory balances = new uint256[](2);
+        receivers[0] = receiver1;
+        receivers[1] = receiver2;
+        balances[0] = 10;
+        balances[1] = 20;
+
+        vm.deal(user, 3000);
+
+        assertEq(user.balance, 3000);
+        assertEq(receivers[0].balance, 0);
+        assertEq(receivers[1].balance, 0);
+
+        vm.prank(user);
+        vm.expectRevert(abi.encodeWithSelector(Sender.TooMuchEther.selector));
+        sender.sendEther{value: 35}(receivers, balances);
+    }
+
+    function testSendEtherToMaxReceivers() public {
+        uint256 maxReceivers = sender.MAX_RECEIVERS();
+        address[] memory receivers = new address[](maxReceivers);
+        uint256[] memory balances = new uint256[](maxReceivers);
+        for (uint160 i = 0; i < maxReceivers; i++) {
+            receivers[i] = address(uint160(receiver1) + i);
+            balances[i] = 10;
+        }
+
+        vm.deal(user, maxReceivers * 100);
+
+        for (uint256 i = 0; i < maxReceivers; i++) {
+            assertEq(receivers[i].balance, 0);
+        }
+
+        vm.prank(user);
+        sender.sendEther{value: maxReceivers * 10}(receivers, balances);
+
+        for (uint256 i = 0; i < maxReceivers; i++) {
+            assertEq(receivers[i].balance, 10);
+        }
+    }
+
+    function testSendTwoTokenToMaxReceivers() public {
+        uint256 maxReceivers = sender.MAX_RECEIVERS();
+        address[] memory receivers = new address[](maxReceivers);
+        uint256[] memory token1Balances = new uint256[](maxReceivers);
+        uint256[] memory token2Balances = new uint256[](maxReceivers);
+        for (uint160 i = 0; i < maxReceivers; i++) {
+            receivers[i] = address(uint160(receiver1) + i);
+            token1Balances[i] = 10;
+            token2Balances[i] = 20;
+        }
+
+        vm.deal(user, 1e18);
+
+        vm.prank(user);
+        mock1.mint(user, 10 * maxReceivers);
+        vm.prank(user);
+        mock1.approve(address(sender), 10 * maxReceivers);
+
+        mock2.mint(user, 20 * maxReceivers);
+        vm.prank(user);
+        mock2.approve(address(sender), 20 * maxReceivers);
+        assertEq(mock2.allowance(user, address(sender)), 20 * maxReceivers);
+
+        for (uint256 i = 0; i < maxReceivers; i++) {
+            assertEq(mock1.balanceOf(receivers[i]), 0);
+            assertEq(mock2.balanceOf(receivers[i]), 0);
+        }
+
+        vm.prank(user);
+        sender.sendTwoToken(address(mock1), address(mock2), receivers, token1Balances, token2Balances);
+
+        for (uint256 i = 0; i < maxReceivers; i++) {
+            assertEq(mock1.balanceOf(receivers[i]), 10);
+            assertEq(mock2.balanceOf(receivers[i]), 20);
+        }
+    }
+
+    function testTooManyReceivers() public {
+        uint256 maxReceivers = sender.MAX_RECEIVERS();
+        address[] memory receivers = new address[](maxReceivers + 1);
+        uint256[] memory balances = new uint256[](maxReceivers + 1);
+        for (uint160 i = 0; i < maxReceivers + 1; i++) {
+            receivers[i] = address(uint160(receiver1) + i);
+            balances[i] = 10;
+        }
+
+        vm.deal(user, 3000);
+
+        for (uint256 i = 0; i < maxReceivers + 1; i++) {
+            assertEq(receivers[i].balance, 0);
+        }
+
+        vm.prank(user);
+        vm.expectRevert(abi.encodeWithSelector(Sender.TooManyReceivers.selector));
+        sender.sendEther{value: 35}(receivers, balances);
+
+        for (uint256 i = 0; i < maxReceivers + 1; i++) {
+            assertEq(receivers[i].balance, 0);
+        }
     }
 
     function printUserBalance(address _user) internal {
