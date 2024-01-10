f1bba46996a8121497bcdec6aa6c5e3cd4ae04b3
kafeikui <kkffkk3@protonmail.com>
2024-01-10 12:07:25 +0800

    fix

--- src/Sender.sol
+++ src/Sender.sol
@@ -7,23 +7,26 @@ import {IERC20, SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/ut
 contract Sender is Ownable {
     using SafeERC20 for IERC20;
 
+    uint256 public constant MAX_RECEIVERS = 60;
+
     event TokenSended(address indexed sender, address indexed tokenAddress, uint256 ethTotal, uint256 tokenTotal);
     event ClaimedTokens(address indexed token, address owner, uint256 balance);
 
     error TooManyReceivers();
-    error NotEnoughBalance();
+    error NotEnoughEther();
+    error TooMuchEther();
 
     constructor() Ownable(msg.sender) {}
 
     function sendToken(address token, address[] calldata receivers, uint256[] calldata balances) public {
         uint256 total = 0;
-        if (receivers.length > 100) {
+        if (receivers.length > MAX_RECEIVERS) {
             revert TooManyReceivers();
         }
         IERC20 erc20token = IERC20(token);
         for (uint256 i = 0; i < receivers.length; i++) {
-            erc20token.transferFrom(msg.sender, receivers[i], balances[i]);
             total += balances[i];
+            erc20token.safeTransferFrom(msg.sender, receivers[i], balances[i]);
         }
         emit TokenSended(msg.sender, token, 0, total);
     }
@@ -37,34 +40,37 @@ contract Sender is Ownable {
     ) public {
         uint256 total1 = 0;
         uint256 total2 = 0;
-        if (receivers.length > 100) {
+        if (receivers.length > MAX_RECEIVERS) {
             revert TooManyReceivers();
         }
         IERC20 erc20token1 = IERC20(token1);
         IERC20 erc20token2 = IERC20(token2);
         for (uint256 i = 0; i < receivers.length; i++) {
-            erc20token1.transferFrom(msg.sender, receivers[i], token1Balances[i]);
-            erc20token2.transferFrom(msg.sender, receivers[i], token2Balances[i]);
             total1 += token1Balances[i];
             total2 += token2Balances[i];
+            erc20token1.safeTransferFrom(msg.sender, receivers[i], token1Balances[i]);
+            erc20token2.safeTransferFrom(msg.sender, receivers[i], token2Balances[i]);
         }
         emit TokenSended(msg.sender, token1, 0, total1);
         emit TokenSended(msg.sender, token2, 0, total2);
     }
 
     function sendEther(address[] calldata receivers, uint256[] calldata balances) public payable {
-        uint256 total = msg.value;
-        if (receivers.length > 100) {
+        uint256 ethLeft = msg.value;
+        if (receivers.length > MAX_RECEIVERS) {
             revert TooManyReceivers();
         }
         for (uint256 i = 0; i < receivers.length; i++) {
-            if (total < balances[i]) {
-                revert NotEnoughBalance();
+            if (ethLeft < balances[i]) {
+                revert NotEnoughEther();
             }
-            total = total - balances[i];
+            ethLeft -= balances[i];
             payable(receivers[i]).transfer(balances[i]);
         }
-        emit TokenSended(msg.sender, address(0x0), msg.value - total, 0);
+        if (ethLeft > 0) {
+            revert TooMuchEther();
+        }
+        emit TokenSended(msg.sender, address(0x0), msg.value, 0);
     }
 
     function sendTokenAndEther(
@@ -73,22 +79,25 @@ contract Sender is Ownable {
         uint256[] calldata tokenBalances,
         uint256[] calldata etherBalances
     ) public payable {
-        uint256 ethTotal = msg.value;
+        uint256 ethLeft = msg.value;
         uint256 tokenTotal = 0;
-        if (receivers.length > 100) {
+        if (receivers.length > MAX_RECEIVERS) {
             revert TooManyReceivers();
         }
         IERC20 erc20token = IERC20(token);
         for (uint256 i = 0; i < receivers.length; i++) {
-            if (ethTotal < etherBalances[i]) {
-                revert NotEnoughBalance();
+            if (ethLeft < etherBalances[i]) {
+                revert NotEnoughEther();
             }
-            ethTotal -= etherBalances[i];
+            ethLeft -= etherBalances[i];
             tokenTotal += tokenBalances[i];
             payable(receivers[i]).transfer(etherBalances[i]);
-            erc20token.transferFrom(msg.sender, receivers[i], tokenBalances[i]);
+            erc20token.safeTransferFrom(msg.sender, receivers[i], tokenBalances[i]);
         }
-        emit TokenSended(msg.sender, token, msg.value - ethTotal, tokenTotal);
+        if (ethLeft > 0) {
+            revert TooMuchEther();
+        }
+        emit TokenSended(msg.sender, token, msg.value, tokenTotal);
     }
 
     function claimTokens(address token) public onlyOwner {
@@ -98,7 +107,7 @@ contract Sender is Ownable {
         }
         IERC20 erc20token = IERC20(token);
         uint256 balance = erc20token.balanceOf(address(this));
-        erc20token.transfer(msg.sender, balance);
+        erc20token.safeTransfer(msg.sender, balance);
         emit ClaimedTokens(token, msg.sender, balance);
     }
 }
